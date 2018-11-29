0b142b770157e35d94a6bd3dad8307fbbeb9f424
Madonahs <syombuamadona@gmail.com>
2018-11-29 10:24:18 -0600

    1.1.1

--- Algorithms/src/com/madonasyombua/algorithms/Exe01.java
+++ Algorithms/src/com/madonasyombua/algorithms/Exe01.java
@@ -5,6 +5,21 @@ public class Exe01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		getValue();
+		
+	}
+	/**
+	 * 1.1.1 Give the value of each of the following expressions:
+	 */
+	static void getValue() {
+	
+		int a = (0 + 15)/2;
+		double b = 2.0e-6 * 100000000.1;
+		boolean c = true && false || true && true;
+		
+		System.out.printf("The answer is: %d\n", a);
+		System.out.printf("The answer is: %f\n", b);
+		System.out.printf("The answer is: %s\n", c);
 	}
 
 }
